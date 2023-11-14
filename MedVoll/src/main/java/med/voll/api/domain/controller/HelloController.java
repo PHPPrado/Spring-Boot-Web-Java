1485a299efd578945b1f03ef6c24ed3b8d7e3e97
Pedro <pedro.phprado@gmail.com>
2023-11-14 11:22:51 -0300

    Feat: Tratando erros 404 e 400

--- MedVoll/src/main/java/med/voll/api/domain/controller/HelloController.java
+++ MedVoll/src/main/java/med/voll/api/domain/controller/HelloController.java
@@ -1,4 +1,4 @@
-package med.voll.api.controller;
+package med.voll.api.domain.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- MedVoll/src/main/java/med/voll/api/domain/controller/MedicoController.java
+++ MedVoll/src/main/java/med/voll/api/domain/controller/MedicoController.java
@@ -1,8 +1,8 @@
-package med.voll.api.controller;
+package med.voll.api.domain.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.*;
-import org.apache.coyote.Response;
+import med.voll.api.domain.medico.*;
+import med.voll.api.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
